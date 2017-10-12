ba72bbfc0e37bcd3548343979933f2d5f453d36e
LironSaedi <lionsaedi@gmail.com>
2017-10-11 17:28:34 -0700

    I learned how to control the suervos. Next Steps are to contine making the functions for open and close

--- LironSaediGarageProject.ino
+++ LironSaediGarageProject.ino
@@ -1,4 +1,6 @@
-int delayv = 300;
+#include <Servo.h>
+
+int delayv = 700;
 int a = 2;
 int b = 3;
 int c = 4;
@@ -6,7 +8,34 @@ int d = 5;
 int e = 6;
 int f = 7;
 int g = 8;
+Servo leftArm;
+Servo rightArm;
+void rightArmOpen()
+{
+  rightArm.write(90);
+}
+void leftArmOpen()
+{
+  leftArm.write(90);
+}
 
+void rightArmClose()
+{
+  rightArm.write(10);
+}
+void leftArmClose()
+{
+  leftArm.write(170);
+}
+void Zero()
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+}
 void One()
 {
   digitalWrite(b, HIGH);
@@ -55,47 +84,96 @@ void Six()
 
 }
 void Seven()
-{digitalWrite(a, OUTPUT);
-  digitalWrite(b, OUTPUT);
-  digitalWrite(c, OUTPUT);
+{ digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
 }
 void Eight()
 
 {
-  digitalWrite(a, OUTPUT);
-  digitalWrite(b, OUTPUT);
-  digitalWrite(c, OUTPUT);
-  digitalWrite(d, OUTPUT);
-  digitalWrite(e, OUTPUT);
-  digitalWrite(f, OUTPUT);
-  digitalWrite(g, OUTPUT);
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
 }
 void Nine()
 {
-  digitalWrite(a, OUTPUT);
-  digitalWrite(b OUTPUT);
-  digitalWrite(c, OUTPUT);
-  digitalWrite(g, OUTPUT);
-  digitalWrite(f, OUTPUT);
+  digitalWrite(a, HIGH);
+  digitalWrite(b , HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(f, HIGH);
+}
+void Off()
+{
+
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
 }
 
 
 void setup()
 {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  leftArm.attach(10);
+  rightArm.attach(9);
+  //pinMode(Nine,OUTPUT);
   // put your setup code here, to run once:
-  
+
 
 }
 
 void loop() {
+  Zero();
+  delay(delayv);
+  Off();
+  One();
+  delay(delayv);
+  Off();
+  Two();
+  delay(delayv);
+  Off();
+  Three();
+  delay(delayv);
+  Off();
+  Four();
+  delay(delayv);
+  Off();
+  Five();
+  delay(delayv);
+  Off();
+  Six();
+  delay(delayv);
+  Off();
+  Seven();
+  delay(delayv);
+  Off();
+  Eight();
+  delay(delayv);
+  Off();
+  Nine();
+  delay(delayv);
+  Off();
+  rightArmOpen();
+  leftArmClose(); 
+
   // put your main code here, to run repeatedly:
   //  for (int i = 0; i < 9; i++)
   //  { digitalWrite(i, HIGH);
   //
   //  }
-  Nine();
-  delay(dealyv);
-  Eight();
-  delay(dealyv);
-
 }
